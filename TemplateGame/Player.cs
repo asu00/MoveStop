0a43e9bcffc725445d9e58cb2b37061c436a8584
name <you@gmail.com>
2019-10-23 10:36:02 +0900

    title

---
+++ TemplateGame/Player.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace OneButton
+{
+    class Player
+    {
+        Size size = new Size();
+        const int SIZE_PLAYER = 64;
+
+        Vector2 pos,posPre;
+        int speed;
+       const int RUDIOS = 32;
+
+        //***
+        const int HIGH_SPEED = 6;
+        const int NORMAL_SPEED = 2;
+        int sc;
+        bool drop,accele,accelePre;
+        public bool DropF => drop;
+
+        public int SC { get { return sc; } }
+        public Vector2 Pos { get { return pos; } }
+        public Vector2 PosPre { get { return posPre; } }
+        public int R => RUDIOS;
+
+        enum State { drop, fly,stop,acccel,dead}
+        State state;
+        State statePre;
+
+        public int St { get { return (int)state; } }
+        public int StPre { get { return (int)statePre; } }
+        public bool Ac { get { return accele; } }
+        public bool AcPre { get { return accelePre; } }
+
+        public Player() { Ini(); }
+        public void Ini()
+        {
+            pos = new Vector2(size.Width/2 - SIZE_PLAYER/2,0);
+            posPre = new Vector2(size.Width / 2 - SIZE_PLAYER / 2, 0);
+            state = State.drop;
+            statePre = state;
+            //***
+            speed = HIGH_SPEED;
+            sc = 0;
+            drop = true ;
+            accele = false;
+            accelePre = accele;
+        }
+        public void Update(Key key)
+        {
+            posPre = pos;
+            statePre = state;
+            KeyPushMove(key);
+            if (!drop) state = State.fly;
+        }
+        public void KeyPushMove(Key key)
+        {
+            accelePre = accele;
+            if (key.TwoPush) accele = true;
+            if (accele)
+            {
+                drop = true;
+                //***
+                speed = HIGH_SPEED;
+            }
+            //***
+            else speed = NORMAL_SPEED;
+            if (key.OnePush)
+            {
+                switch (drop)
+                {
+                    case true:
+                        drop = false;
+                        accele = false;
+                        break;
+                    case false:
+                        drop = true;
+                        accele = false;
+                        break;
+                }
+            }
+            Debug.WriteLine("加速" + accele);
+        }
+        public void Drop()
+        {
+            if (drop)
+            {
+                pos.Y += speed;
+                state = State.drop;
+            }
+            if (pos.Y > size.Under && drop && pos.Y <= size.EndSc) sc += speed;
+        }
+        public void FloorMove(float move)
+        {
+            pos.X += move;
+            state = State.stop;
+            accele = false;
+        }
+        public void DeadFlag()
+        {
+            state = State.dead;
+        }
+    }
+}
---
+++ TemplateGame/Size.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneButton
+{
+    class Size
+    {
+        //クラスで共通のサイズプロパティ
+        const int CELL = 64;
+        const int WIDTH = CELL * 9;
+        const int HEIGHT = CELL * 15;
+        const int WIN_WIDTH = WIDTH + CELL;
+        const int UNDER = HEIGHT - CELL * 6;
+
+        const int WORLD = 3000;
+        const int END_COORD = WORLD - CELL * 6;
+
+        public int World { get { return WORLD; } }
+        public int EndSc
+        { get { return END_COORD; } }
+        public int Win_Width { get { return WIN_WIDTH; } }
+        public int Width { get { return WIDTH; } }
+        public int Height { get { return HEIGHT; } }
+        public int Under { get { return UNDER; } }
+    }
+}
